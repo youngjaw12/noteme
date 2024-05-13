adebc0f86d67492f30181b67929369fb4e79e735
jonathan <agahjonathan@gmail.com>
2024-05-13 09:41:51 +0100

    first commit

---
+++ config/tailwind.config.js
@@ -0,0 +1,33 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    colors:{
+     bgcolor: "#F8EEE2",
+     buttoncolor: "#D9614C",
+     black: "#000000",
+     header: "#403B36",
+     text: "#595550",
+     textbg: "#FFFDFA",
+     white: "#ffffff"
+
+    },
+    extend: {
+      fontFamily: {
+        sans: ['sans var', ...defaultTheme.fontFamily.sans],
+        nunito: ['Nunito','sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
