5d290e2f65954c2d3b509b354c51f148e293639e
jonathan <agahjonathan@gmail.com>
2024-06-12 12:00:04 +0100

    added second  styling to note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,7 +3,7 @@ class NotesController < ApplicationController
 
   # GET /notes or /notes.json
   def index
-    @notes = Note.all
+    @notes = Note.all.order("created_at DESC")
   end
 
   # GET /notes/1 or /notes/1.json
