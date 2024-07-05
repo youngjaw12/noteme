824af318c5cb92ae1d3d14a57dfbd7bc061074f6
jonathan <agahjonathan@gmail.com>
2024-07-05 08:44:01 +0100

    added profile dropdown in the nav

---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="dropdown"
+
+export default class extends Controller {
+  static targets = [ "menu" ]
+
+  toggle() {
+    this.menuTarget.classList.toggle("hidden")
+  }
+}
