5242e936fce97d5de4cb034619f1f461eaa12931
jonathan <agahjonathan@gmail.com>
2024-07-24 17:09:51 +0100

    added icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 module ApplicationHelper
+    def svg_icon(name, options = {})
+    file_path = "app/assets/images/icons/#{name}.svg"
+    if File.exist?(file_path)
+      file = File.read(file_path)
+      doc = Nokogiri::HTML::DocumentFragment.parse(file)
+      svg = doc.at_css('svg')
+      options.each { |attr, value| svg[attr.to_s] = value }
+      doc.to_html.html_safe
+    else
+      "(SVG not found: #{name})"
+    end
+  end
  
 end
